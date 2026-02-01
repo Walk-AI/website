9c7c2b9a2e92aa074c2e78d01ba26f1cd9739f22
alexander.shugaev <alexwinner2402@gmail.com>
2026-02-01 22:17:47 +0200

    Clean up Vite config formatting and remove unnecessary comments

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath } from "node:url";
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/website/',
   plugins: [vue()],
-    resolve: {
+  resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     }
-
   },
   css: {
     preprocessorOptions: {
-      scss: {
-        api: "modern-compiler"
-      }
+      scss: { api: "modern-compiler" }
     }
   },
 })
